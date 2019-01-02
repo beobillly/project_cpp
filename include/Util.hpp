95d369de5037a73d9999859058ab8814abac3575
Alerxandra Rogova <thequeenmoustache@gmail.com>
2019-01-02 22:26:23 +0100

    draughts move start

--- include/Util.hpp
+++ include/Util.hpp
@@ -1,7 +1,12 @@
 #include "../src/Rank.cpp"
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int letterToNumber(char c);
 Rank getRankFromChar(char c);
 bool isMaj(char c);
 bool isMin(char c);
 bool coordOk(int height, int length, int x, int y);
+vector<string> split(string s, string delimiter);
